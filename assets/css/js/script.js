81d4d96b2ca4daac56461d33db3e61bd886f235d
kikigoestech <iamtummo13>
2022-07-14 19:09:30 -0400

    document ready

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $("#currentDay").html(moment().format("dddd, MMMM Do"));
 });
+
+// once the user log into the website the document will run 
+
+$(document).ready(function(){
+    $(".saveBtn").on("click", function(){
+        var hour = $(this).parent().attr("id");
+        var txt = $(this).siblings(".description").val();
+
+        localStorage.setItem(hour,txt);
+    });
